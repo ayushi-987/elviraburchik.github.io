650c5011a4dcacde3d5c68f7c53cd4c6512c2b23
Elvira Burchik <elviraburchik@gmail.com>
2020-04-24 19:51:34 +0300

    update main index page

--- Content/index.md
+++ Content/index.md
@@ -1,1 +1,4 @@
-# Welcome to ElviraburchikGithubIo!
+I'm **Elvira Burchik, a software developer from Minsk, Belarus**, with over 5 years of experience in iOS development.<br/><br/>
+Previously, I worked at **[Flo Health](https://flo.health/) - the number one women's health app** in both the Android and Apple app stores with 100 million installs and 30 million monthly active users.<br/><br/>
+I take part in the program committee of local **mobile developers conference called [MobileOptimized](https://moconf.by)**, organize meetups, and **mentor in a [program for women starting a career in IT](http://wita.by)**.<br/><br/>
+In my free time, I brew Chemex coffee and binge-watch Netflix 
--- Sources/ElviraburchikGithubIo/main.swift
+++ Sources/ElviraburchikGithubIo/main.swift
@@ -5,7 +5,7 @@ import Plot
 // This type acts as the configuration for your website.
 struct ElviraburchikGithubIo: Website {
     enum SectionID: String, WebsiteSectionID {
-        // Add the sections that you want your website to contain here:
+        case about
         case posts
     }
 
@@ -15,14 +15,37 @@ struct ElviraburchikGithubIo: Website {
 
     // Update these properties to configure your website:
     var url = URL(string: "https://your-website-url.com")!
-    var name = "ElviraburchikGithubIo"
-    var description = "A description of ElviraburchikGithubIo"
+    var name = "Elvira Burchik"
+    var description = "Hi 👋 I'm Elvira Burchik"
     var language: Language { .english }
     var imagePath: Path? { nil }
 }
 
 // This will generate your website using the built-in Foundation theme:
 //try ElviraburchikGithubIo().publish(withTheme: .foundation)
-try ElviraburchikGithubIo().publish(using: [
-    .deploy(using: .gitHub("ElviraBurchik/elviraburchik.github.io", useSSH: false))
-])
+
+try ElviraburchikGithubIo().publish(withTheme: .website,
+                                    deployedUsing: .gitHub("ElviraBurchik/elviraburchik.github.io", useSSH: false)
+)
+
+//try ElviraburchikGithubIo().publish(using: [
+//    .addDefaultSectionTitles(),
+//    .deploy(using: .gitHub("ElviraBurchik/elviraburchik.github.io", useSSH: false))
+//])
+
+extension PublishingStep where Site == ElviraburchikGithubIo {
+    static func addDefaultSectionTitles() -> Self {
+        .step(named: "Default section titles") { context in
+            context.mutateAllSections { section in
+                guard section.title.isEmpty else { return }
+
+                switch section.id {
+                case .about:
+                    section.title = "About"
+                case .posts:
+                    section.title = "Posts"
+                }
+            }
+        }
+    }
+}
